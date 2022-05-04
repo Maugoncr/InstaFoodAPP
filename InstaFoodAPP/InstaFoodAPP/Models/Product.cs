84f8eb4314d0a257055278b7c9961fedfe9577ae
Mauricio <gonzalezmoralesmauricioo@gmail.com>
2022-05-04 15:02:16 -0600

    mauricio

--- InstaFoodAPP/InstaFoodAPP/Models/Product.cs
+++ InstaFoodAPP/InstaFoodAPP/Models/Product.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net;
 using System.Text;
+using System.Threading.Tasks;
+
 namespace InstaFoodAPP.Models
 {
-    public partial class Product
+    public class Product
     {
+        public RestRequest request { get; set; }
+        const string mimetype = "application/json";
+        const string contentType = "Content-Type";
+
+
         public Product()
         {
             Purchases = new HashSet<Purchase>();
@@ -22,5 +33,69 @@ namespace InstaFoodAPP.Models
 
         public virtual Restaurant IdRestNavigation { get; set; }
         public virtual ICollection<Purchase> Purchases { get; set; }
+
+
+
+        public async Task<ObservableCollection<Product>> GetProductsList()
+        {
+
+            try
+            {
+
+                string routeSufix = string.Format("Products");
+
+                string FinalApiRoute = CnnToAPI.ProductiorRoute + routeSufix;
+
+
+                RestClient client = new RestClient(FinalApiRoute);
+
+                request = new RestRequest(FinalApiRoute, Method.Get);
+
+
+                //agregar la info de seguridad, en este caso api key
+
+                request.AddHeader(CnnToAPI.ApiKeyName, CnnToAPI.ApiKeyValue);
+
+                request.AddHeader(contentType, mimetype);
+
+                RestResponse response = await client.ExecuteAsync(request);
+
+                HttpStatusCode statusCode = response.StatusCode;
+
+                var QList = JsonConvert.DeserializeObject<ObservableCollection<Product>>(response.Content);
+
+
+
+                if (statusCode == HttpStatusCode.OK)
+                {
+                    return QList;
+                }
+                else
+                {
+                    return null;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                string msg = ex.Message;
+
+                throw;
+            }
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
     }
 }
